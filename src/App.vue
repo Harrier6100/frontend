ddb6786698af8bc3627de82a94295e2d583ca38d
TOMO MUTO <to-mutou@post.lintec.co.jp>
2026-08-20 17:02:14 +0900

    [1.00]

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-    <Header />
-    <Breadcrumb />
+    <template v-if="route.name !== 'Login'">
+        <Header />
+        <Breadcrumb />
+    </template>
     <router-view />
     <Confirm />
     <ToastContainer />
     <Loading />
 </template>
+
+<script setup>
+const route = useRoute();
+</script>
