55dc7c62cc5e400edaf242182987d71a78ebb4ad
TOMO MUTO <to-mutou@post.lintec.co.jp>
2026-09-16 15:27:27 +0900

    [1.00]

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
-import router from '@/router';
 import i18n from '@/i18n';
 import { fetchMessages } from '@/i18n/fetchMessages';
+import router from '@/router';
 import can from '@/directives/can';
 import './main.css';
 
 const app = createApp(App);
 app.use(createPinia());
-app.use(router);
 app.use(i18n);
 await fetchMessages();
+app.use(router);
 app.directive('can', can);
 app.mount('#app');
